9f2b861e8d2a4902c43f89a3d9b2bdda350a68a2
Raghav Sood <r@raghavsood.com>
2023-11-26 23:21:42 +0800

    webui: move webui to separate package

--- cmd/utxoexplorer/main.go
+++ cmd/utxoexplorer/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"html/template"
 	"log"
 	"net/http"
 
@@ -10,31 +9,12 @@ import (
 
 func main() {
 	// Initialize the router
+	http.HandleFunc("/", webui.RenderIndex)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FS(static.Static))))
-	http.HandleFunc("/", indexHandler)
 
 	// Start the server
 	log.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
-	}
-}
-
-// indexHandler renders the index page
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("templates/index.gohtml")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	data := struct {
-		Title string
-	}{
-		Title: "Bitcoin Explorer",
-	}
-
-	if err := tmpl.Execute(w, data); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
---
+++ webui/webui.go
@@ -0,0 +1,30 @@
+package webui
+
+import (
+	"html/template"
+	"net/http"
+	"path"
+)
+
+// TemplatesDir is the directory where the HTML templates are stored.
+var TemplatesDir = "templates" // Adjust the path according to your project structure
+
+// RenderIndex handles rendering of the index page.
+func RenderIndex(w http.ResponseWriter, r *http.Request) {
+	tmplPath := path.Join(TemplatesDir, "index.gohtml")
+	tmpl, err := template.ParseFiles(tmplPath)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	data := struct {
+		Title string
+	}{
+		Title: "Bitcoin Explorer",
+	}
+
+	if err := tmpl.Execute(w, data); err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
